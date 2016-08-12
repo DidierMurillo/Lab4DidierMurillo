1c7c462048774b35468bae41694fddbb3a3cd23f
Didier45 <didiermurillo@unitec.edu>
2016-08-12 16:16:08 -0400

    Algunas validaciones

--- DidierMurilloLab4.cpp
+++ DidierMurilloLab4.cpp
@@ -17,7 +17,10 @@ int main()
 
 	}
 	if(Opcion==2){
-		TrianguloPascal Triangulo(4);
+		int Lineas;
+		cout<<"Ingrese el numero de lineas:";
+		cin>>Lineas;
+		TrianguloPascal Triangulo(Lineas);
 		Triangulo.Impresion();
 	}	
 	}while(Opcion!=0); 
@@ -77,28 +80,25 @@ void TrianguloPascal::Impresion(){
 	int Col,Filas, Valor,Distancia;	
 	for (Filas = 0; Filas < NFilas; Filas++)
 	{
-	
-    Valor = 1;
-    for(Distancia = 0; Distancia <NFilas -Filas;Distancia++)
-    {
-        cout<<"  ";
-    }
+		Valor = 1;
+		for(Distancia = 0; Distancia <NFilas -Filas;Distancia++)
+   		{
+        	cout<<"  ";
+   	}
 
-    for (Col = 0; Col <= Filas; Col++)
-    {	
-	if(Valor%2==1){
-		cout<<"   "<<"ღ";
-		Valor = Valor * (Filas - Col) / (Col + 1);
-	}else{
-		cout<<"   "<<"☮ ";
-                Valor = Valor * (Filas - Col) / (Col + 1);
+   	 for (Col = 0; Col <= Filas; Col++)
+   	 {	
+		if(Valor%2==1){
+			cout<<"   "<<"ღ";
+			Valor = Valor * (Filas - Col) / (Col + 1);
+		}else{
+			cout<<"   "<<"☮ ";
+               		Valor = Valor * (Filas - Col) / (Col + 1);
+		}
+    	}
+	cout<<"\n";
 	}
-    }
-    cout<<"\n";
-  }
-  cout<<"\n";
-
- 
+ 	cout<<"\n";
 }
 
 
